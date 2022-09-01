9823287ea4dd177ca0017860542d13e4e8eb6c81
devthejo <jo@surikat.pro>
2022-09-01 19:21:31 +0200

    fix: drop-db close sessions

--- jobs/drop-db/drop-db.sh
+++ jobs/drop-db/drop-db.sh
@@ -11,4 +11,13 @@ done
 
 PGPORT=${PGPORT:-5432}
 
+echo "disconnect activities on database ${DATABASE} on ${PGHOST}"
+psql -abe -c "
+  SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE;
+  -- Disconnect users from database
+  SELECT pg_terminate_backend (pg_stat_activity.pid)
+  FROM pg_stat_activity
+  WHERE pg_stat_activity.datname = '${DATABASE}';
+"
+
 dropdb "$DATABASE" || true
